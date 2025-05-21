10e2fca3829adefd591fc2280398fd8dbf84d7d3
SN-44 <nics96@hotmail.com>
2025-05-21 09:19:19 +0200

    Update folders.js

--- folders.js
+++ folders.js
@@ -1,24 +1,22 @@
-// folders.js
-
-function loadFolders() {
-  return JSON.parse(localStorage.getItem('folders')) || [];
-}
-
-function saveFolders(folders) {
-  localStorage.setItem('folders', JSON.stringify(folders));
-}
-
-function deleteFolder(id) {
-  let folders = loadFolders();
-  folders = folders.filter(folder => folder.id !== id);
-  saveFolders(folders);
-}
-
-function updateFolderCheck(id, key, value) {
-  let folders = loadFolders();
-  const folder = folders.find(f => f.id === id);
-  if (folder) {
-    folder.checkedStates[key] = value;
-    saveFolders(folders);
+[
+  {
+    name: "Nom du dossier 1",
+    visiteTechnique: {
+      intervention: "2025-05-21",
+      equipe: "Équipe A",
+      contact: "Jean Dupont",
+      typeRaccordement: "Autoconsommation",
+      puissanceRaccordement: "6 kW",
+      puissanceProjetPV: "5.5 kW",
+      pdlPose: "oui",
+      pdlCommentaire: "Pose faite au chantier A",
+      pdlNombre: "1"
+    }
+  },
+  {
+    name: "Nom dossier 2",
+    visiteTechnique: {
+      // ...
+    }
   }
-}
+]
